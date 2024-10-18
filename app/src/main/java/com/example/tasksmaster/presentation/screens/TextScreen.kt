b4ada5938dcaac989b108f9bda34c19611f117c5
Игорь <Igorkoynov2001@gmail.com>
2024-10-18 21:35:49 +0700

    fix bug

--- app/src/main/java/com/example/tasksmaster/presentation/screens/TextScreen.kt
+++ app/src/main/java/com/example/tasksmaster/presentation/screens/TextScreen.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -17,6 +19,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
@@ -103,6 +106,7 @@ fun TextScreen(
                             fontSize = 12.sp,
                             color = Color.Gray,
                         )
+                        Text("Hello")
                     }
                 },
                 navigationIcon = {
